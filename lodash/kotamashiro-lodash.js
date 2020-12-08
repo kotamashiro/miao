330f88580fcc1ac3a1da4348f923140f744ce9b9
dianjuren <kotamashiro@outlook.com>
2020-12-08 16:23:30 +0800

    lodash-4

--- lodash/kotamashiro-lodash.js
+++ lodash/kotamashiro-lodash.js
@@ -15,7 +15,44 @@ var kotamashiro = function () {
     }
     return result
   }
+  function difference(ary, val) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      for (var j = 0; i < val.length; j++) {
+        if (ary[i] == val[j]) {
+          ary.shift(ary[i])
+          result = ary
+        }
+      }
+    }
+    return result
+  }
+
+  function concat(ary, val) {
+    var result = []
+    result = ary
+    for (var i = 0; i < val.length; i++) {
+      result.push(ary[i])
+    }
+    return result
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return {
-    compact, chunk,
+    compact, chunk, difference, concat,
   }
 }()
